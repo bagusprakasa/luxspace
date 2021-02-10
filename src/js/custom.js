7d8927dfb99e300d1371bd000ff77f54d996379e
Galih Bagus Prakasa <galihbagus555@gmail.com>
2021-02-10 09:17:41 +0700

    browse the room
    add anchor link smooth and browse the room

--- src/js/custom.js
+++ src/js/custom.js
@@ -45,3 +45,19 @@ for (let index = 0; index < modalTriggers.length; index++) {
         document.body.append(modalWrapper);
     });
 }
+
+// anchor link smooth
+const smoothScrollAnchor = document.querySelectorAll("a[href^='#']");
+
+for (let index = 0; index < smoothScrollAnchor.length; index++) {
+  const el = smoothScrollAnchor[index];
+
+  el.addEventListener("click", function (ev) {
+    ev.preventDefault();
+
+    if (document.getElementById(this.getAttribute("href").replace("#", "")))
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+  });
+}
